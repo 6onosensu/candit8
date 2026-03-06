74d28250cc42d032305af7edd37eed83e3636333
darja <darja.portnova@gmail.com>
2026-03-06 15:12:57 +0100

    feat: add users controller; add user service with stub methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Body, Controller, Delete, Get, 
+  HttpCode, HttpStatus, Param, 
+  Patch, Post, Put 
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateUserDto): Promise<User> {
+    return this.usersService.create(dto);
+  }
+  
+  @Patch(':id') 
+  patch(@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<User> {
+    return this.usersService.patch(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id') id: string): Promise<void> {
+    return this.usersService.delete(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User, UserRole } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async findAll(): Promise<User[]> {
+    return [];
+  }
+
+  async findOne(id: string): Promise<User> {
+    return { id } as User;
+  }
+
+  async create(dto: CreateUserDto): Promise<User> {
+    return { ...dto, role: UserRole.DEVELOPER, isActive: true } as User;
+  }
+
+  async patch(id: string, dto: UpdateUserDto): Promise<User> {
+    return { ...dto, id } as User;
+  } 
+
+  async delete(id: string): Promise<void>{
+    return;
+  }
+}
