27583f6be7657bc877ea7560bebe9727c7d52134
darja <darja.portnova@gmail.com>
2026-05-10 15:45:31 +0200

    add health module and config module with validation and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,30 +3,37 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HealthController } from './modules/health/health.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HealthModule } from './modules/health/health.module';
+import { validationSchema } from './config/validation.schema';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
+      validationSchema,
     }),
+
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
+        host: config.getOrThrow<string>('database.host'),
+        port: config.getOrThrow<number>('database.port'),
+        username: config.getOrThrow<string>('database.username'),
+        password: config.getOrThrow<string>('database.password'),
+        database: config.getOrThrow<string>('database.name'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: false,
       }),
     }),
+
     UsersModule,
+    HealthModule,
   ],
-  controllers: [AppController, HealthController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
